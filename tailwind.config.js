f69c0bdea036f5150612e0977783829ca24fcea4
arodriguez201 <85238832+arodriguez201@users.noreply.github.com>
2023-09-05 16:00:22 -0400

    configured tailwind.config, index.html w/ design edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./main/*.{html,css,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cormorant-garamond': ['Cormorant Garamond', 'serif'],
+        'gfs-didot': ['GFS Didot', 'serif'],
+        'playfair-display-sc': ['Playfair Display SC', 'serif'],
+      },
+      backgroundImage: {
+        'fancy-restaurant': "url('/main/images/fancy.avif')",
+        'fancy-pattern': "url('/main/patterns/fancy-cushion.webp')",
+      },
+      screens: {
+        'xs': '320px',
+        'xxs': '270px',
+      },
+    },
   },
   plugins: [],
 }
