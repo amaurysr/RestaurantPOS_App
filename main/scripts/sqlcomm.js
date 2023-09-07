6f58b987d3e8b7963201e8580489a9080272d9f7
arodriguez201 <85238832+arodriguez201@users.noreply.github.com>
2023-09-07 17:00:36 -0400

    added sqlcomm.js file

---
+++ main/scripts/sqlcomm.js
@@ -0,0 +1,31 @@
+const mysql2 = require('mysql2'); 
+const express = require('express'); 
+const app = express(); 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json);
+
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '04D@ckdocko123',
+    database: 'RestaurantPOSDB',
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Database connection error: ', err);
+      return;
+    }
+  });
+
+app.post('/main/scripts/sqlcomm.js', (req/res) => {
+    const {email, password, firstname, lastname, username} = req.body; 
+
+});
+
+connection.query()
+
+connection.end()
+  
